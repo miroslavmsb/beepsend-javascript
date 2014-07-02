4204ec25c0f55195708cdaeaa5eecb5a5f3313bb
Miroslav Milosevic <m.maksa@gmail.com>
2014-07-02 11:52:03 +0200

    refactoring - creating separate classes for each group of endpoints

--- src/beepsend.js
+++ src/beepsend.js
@@ -678,10 +678,351 @@ beepsend.prototype = (function() {
             this.api.resource('/wallets/', "GET", {}, success, error);
         },
         
+        user: function()
+        {
+            return new beepsend.user(this);
+        },
+        
+        analytic: function()
+        {
+            return new beepsend.analytic(this);
+        }
+        
     };
     
     
 })();
+
+beepsend.user = function(bs)
+{
+    this.parameters = bs.parameters;
+    this.api = bs.api;
+    
+    this.user = "me";
+    
+    this.actions = {
+        'users' : '/users/',
+        'email' : 'email',
+        'phone' : 'phone',
+        'password' : 'password',
+        'passwordreset' : 'passwordreset',
+        'tokenreset' : '/tokenreset'
+    };
+}
+
+beepsend.user.prototype = {
+    
+    /**
+     * Get user details
+     * @param {function} success - callback function for handling success response
+     * @param {function} error - callback function for handling error
+     * @returns {object} - user object
+     */
+    data: function(success, error)
+    {
+        this.api.resource(this.actions.users+this.user, "GET", {}, success, error);
+    },
+    
+    /**
+     * Update user
+     * @param {object} options object for user that we want to update
+     * @param {function} success - callback function for handling success response
+     * @param {function} error - callback function for handling error
+     * @returns {object} updated user object
+     * @link http://api.beepsend.com/docs.html#user-update
+     */
+    update: function(options, success, error)
+    {
+        this.api.resource(this.actions.users+this.user, "PUT", options, success, error);
+    },
+    
+    /**
+     * Update user email
+     * @param {string} email - new email address
+     * @param {string} password - password of user account
+     * @param {function} success - callback function for handling success response
+     * @param {function} error - callback function for handling error
+     * @returns {object}
+     */
+    updateEmail: function(email, password, success, error)
+    {
+        var data = {
+            "email" : email,
+            "password" : password
+        };
+        this.api.resource(this.actions.users+this.user+'/'+this.actions.email, "PUT", data, success, error);
+    },
+    
+    /**
+     * Update user password
+     * @param {string} oldPassword
+     * @param {string} newPassword
+     * @param {function} success - callback function for handling success response
+     * @param {function} error - callback function for handling error
+     * @returns {object}
+     */
+    updatePassword: function(oldPassword, newPassword, success, error)
+    {
+        var data = {
+            'password' : oldPassword,
+            'new_password' : newPassword
+        };
+        this.api.resource(this.actions.users+this.user+'/'+this.actions.password, "PUT", data, success, error);
+    },
+    
+    /**
+     * Reset user password
+     * @param {string} email - email for password reset
+     * @param {function} success - callback function for handling success response
+     * @param {function} error - callback function for handling error
+     * @returns {object} user object
+     */
+    resetUserPassword: function(email, success, error)
+    {
+        var data = {
+            "email" : email
+        };
+        this.api.resource(this.actions.users+this.actions.passwordreset, "GET", data, success, error);
+    },
+    
+    /**
+     * Set new password
+     * @param {string} hash - is sent to you by email address
+     * @param {string} password - new password
+     * @param {function} success - callback function for handling success response
+     * @param {function} error - callback function for handling error
+     * @returns {object} user object
+     */
+    setNewPassword: function(hash, password, success, error)
+    {
+        var data = {
+            'password' : password
+        };
+        this.api.resource(this.actions.users+this.actions.password+'/'+hash, "PUT", data, success, error);
+    },
+    
+    /**
+     * Reset user token
+     * @param {string} password - user password
+     * @param {function} success - callback function for handling success response
+     * @param {function} error - callback function for handling error
+     * @returns {object} new user token
+     */
+    resetUserToken: function(password, success, error)
+    {
+        var data = {
+            'password' : password
+        };
+        this.api.resource(this.actions.users+this.user+this.actions.tokenreset, "GET", data, success, error);
+    },
+    
+    /**
+     * Verify email address
+     * @param {string} hash - is sent to you via email
+     * @param {function} success - callback function for handling success response
+     * @param {function} error - callback function for handling error
+     * @returns {object}
+     */
+    verifyEmail: function(hash, success, error)
+    {
+        this.api.resource(this.actions.users+this.actions.email+'/'+hash, "GET", {}, success, error);
+    },
+    
+    /**
+     * Verify phone number
+     * @param {type} hash - has from sms message
+     * @param {function} success - callback function for handling success response
+     * @param {function} error - callback function for handling error
+     * @returns {object}
+     */
+    verifyPhone: function(hash, success, error)
+    {
+        this.api.resource(this.actions.users+this.actions.phone+'/'+hash, "GET", {}, success, error);
+    }
+    
+};
+
+
+beepsend.analytic = function(bs)
+{
+    this.parameters = bs.parameters;
+    this.api = bs.api;
+    
+    
+    this.actions = {
+        'summary' : '/analytics/summary/',
+        'network' : '/analytics/network/',
+        'batch' : '/analytics/batches/'
+    };
+};
+
+beepsend.analytic.prototype = {
+    
+    /**
+     * Get accumulated Statistics, need to set connection id for this
+     * @param {string} connection - connection id or string "me"
+     * @param {int} fromDate - Unix time
+     * @param {int} toDate - Unix time
+     * @param {function} success - callback function for handling success response
+     * @param {function} error - callback function for handling error
+     * @returns {object}
+     */
+    summary: function(connection, fromDate, toDate, success, error)
+    {
+        connection = connection || '';
+        fromDate = fromDate || null;
+        toDate = toDate || null;
+        
+        var data = {};
+        
+        if(fromDate !== null) {
+            data.from_date = fromDate;
+        }
+        
+        if(toDate !== null) {
+            data.to_date = toDate;
+        }
+        
+        this.api.resource(this.actions.summary+connection, "GET", data, success, error);
+    },
+    
+    /**
+     * This call will give you granular delivery statistics for all messages sorted by each recipient network between two specified dates.
+     * @param {sring} connection - connection id
+     * @param {int} fromDate - Unix time
+     * @param {int} toDate - Unix time
+     * @param {string} MCC - Mobile country code
+     * @param {string} MNC - Mobile network code
+     * @param {function} success - callback function for handling success response
+     * @param {function} error - callback function for handling error
+     * @returns {object}
+     */
+    network: function(connection, fromDate, toDate, MCC, MNC, success, error)
+    {
+        connection = connection || null;
+        fromDate = fromDate || null;
+        toDate = toDate || null;
+        MCC = MCC || null;
+        MNC = MNC || null;
+        
+        var data = {};
+        
+        if(fromDate !== null) {
+            data.from_date = fromDate;
+        }
+        
+        if(toDate !== null) {
+            data.to_date = toDate;
+        }
+        
+        if(MCC !== null) {
+            data.MCC = MCC;
+        }
+        
+        if(MNC !== null) {
+            data.MNC = MNC;
+        }
+        
+        this.api.resource(this.actions.network+connection, "GET", data, success, error);
+        
+    },
+    
+    /**
+     * This call will give you delivery statistics for a whole batch
+     * @param {int} batchId - batch id
+     * @param {function} success - callback function for handling success response
+     * @param {function} error - callback function for handling error
+     * @returns {undefined}
+     */
+    batch: function(batchId, success, error)
+    {
+        batchId = batchId || null;
+        this.api.resource(this.actions.batch+batchId, "GET", {}, success, error);
+    }
+    
+};
+
+beepsend.connection = function(bs)
+{
+    this.parameters = bs.parameters;
+    this.api = bs.api;
+    
+    this.actions = {
+        'connections' : '/connections/',
+        'tokenreset' : '/tokenreset',
+        'passwordreset' : '/passwordreset'
+    };
+    
+};
+
+beepsend.connection.prototype = {
+    
+    /**
+     * Get all connections
+     * @param {function} success - callback function for handling success response
+     * @param {function} error - callback function for handling error
+     * @returns {collection} collection with all connection objects
+     */
+    all: function(success, error) 
+    {
+        this.api.resource(this.actions.connections, "GET", {}, success, error);
+    },
+    
+    /**
+     * Get data for single connection
+     * @param {string} connection - connection id or "me" string
+     * @param {function} success - callback function for handling success response
+     * @param {function} error - callback function for handling error
+     * @returns {undefined}
+     */
+    data: function(connection, success, error)
+    {
+        connection = connection || 'me';
+        this.api.resource(this.actions.connections+connection, "GET", {}, success, error);
+    },
+    
+    /**
+     * Update connection
+     * @param {string} connection - connection id or "me" string if we want to update authorized user connection
+     * @param {type} options - options that we want to update
+     * @param {function} success - callback function for handling success response
+     * @param {function} error - callback function for handling error
+     * @returns {undefined}
+     * @description for details about available options for updating see api documentation
+     * @link http://api.beepsend.com/docs.html#connection-update
+     */
+    update: function(connection, options, success, error)
+    {
+        connection = connection || 'me';
+        this.api.resource(this.actions.connections+connection, "PUT", options, success, error);
+    },
+    
+    /**
+     * Reset connection token, need to use user token to perform this action
+     * @param {string} connection - connection id or "me" string
+     * @param {function} success - callback function for handling success response
+     * @param {function} error - callback function for handling error
+     * @returns {object} json response with new token
+     */
+    resetToken: function(connection, success, error)
+    {
+        this.api.resource(this.actions.connections+connection+this.actions.tokenreset, "GET", {}, success, error);
+    },
+    
+    /**
+     * Reset connection password
+     * @param {type} connection - connection id or "me" string
+     * @param {function} success - callback function for handling success response
+     * @param {function} error - callback function for handling error
+     * @returns {object}
+     */
+    resetPassword: function(connection, success, error)
+    {
+        connection = connection || "me";
+        this.api.resource(this.actions.connection+connection+this.actions.passwordreset, "GET", {}, success, error);
+    }
+    
+};
 
 /**
  * Beepsend api communication class
@@ -710,6 +1051,16 @@ beepsend.api.prototype = {
         return url;
     },
     
+    resource: function(resource, type, data, callback, error)
+    {
+//        if(typeof $ == 'undefined') {
+            this.resourceHandler(resource, type, data, callback, error);
+//        }
+//        else {
+//            this.resourceJquery(resource, type, data, callback, error);
+//        }
+    },
+    
     /**
      * Resource function
      * @param {string} resource - this is resource string
@@ -718,7 +1069,49 @@ beepsend.api.prototype = {
      * @param {string} callback - this is name of success callback function to process received data
      * @param {string} error - this is name of error callback function to process errors on requests
      */
-    resource: function(resource, type, data, callback, error) {
+    resourceHandler: function(resource, type, data, callback, error)
+    {
+        if (window.XMLHttpRequest){
+            var xhr = new XMLHttpRequest();   
+        } else {
+            var xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        };
+        
+        callback = callback || this.successCallback;
+        error = error || this.errorCallback;
+        
+        var fullResourceUrl = this.buildRequestUrl(resource);
+        
+        if(type== "GET") {
+            var qs = (this.serialize(data).length > 0) ? '&'+this.serialize(data) : this.serialize(data);
+            fullResourceUrl = fullResourceUrl+qs;
+        }
+        
+        xhr.open(type, fullResourceUrl, true);
+        
+        if(type == "POST" || type == "PUT" || type == "DELETE") {
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+//            xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+//            xhr.setRequestHeader("Content-length", data.length);
+//            xhr.setRequestHeader("Connection", "close");
+            xhr.send(this.serialize(data));
+        }
+        
+        else {
+            xhr.send();
+        }       
+        
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState == 4 && xhr.status == 200) {
+                callback(JSON.parse(xhr.responseText));
+            }
+            else {
+//                error(xhr, xhr.status, resource);
+            }
+        }
+        
+    },
+    resourceJquery: function(resource, type, data, callback, error) {
         /* Set default handler functions */
         callback = callback || this.successCallback;
         error = error || this.errorCallback;
@@ -762,6 +1155,23 @@ beepsend.api.prototype = {
     },
     
     /**
+     * Function for serializing json object to query string
+     * @param {object} obj - object that we want to serialize
+     * @param {string} prefix for all object keys
+     * @returns {String} query string
+     */
+    serialize: function(obj, prefix) {
+        var str = [];
+        for(var p in obj) {
+          var k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
+          str.push(typeof v == "object" ?
+            this.serialize(v, k) :
+            encodeURIComponent(k) + "=" + encodeURIComponent(v));
+        }
+        return str.join("&");
+    },
+    
+    /**
      * Default success callback function
      * @param {object} data - response data
      */
@@ -773,7 +1183,7 @@ beepsend.api.prototype = {
      * Default error callback function
      * @param {object} error - error object
      */
-    errorCallback: function(xhr, status) {
+    errorCallback: function(xhr, status, resource) {
         switch (xhr.status) {
             case 404:
                 console.log('Error: Not found');
@@ -785,7 +1195,7 @@ beepsend.api.prototype = {
                 console.log('Error: Request aborted');
                 break;
             default:
-                console.log('Error: Unknown error ' + (typeof xhr.responseJSON.errors != "undefined") ? xhr.responseJSON.errors.toString() : status);
+                console.log('Error: Unknown error ' + status);
                 
         } 
     },
